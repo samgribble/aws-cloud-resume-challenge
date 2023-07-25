290540cf5a36a2a0ccb4c053e506cedadae6733b
samgribble <samuel.gribble@yahoo.com>
2023-07-25 14:30:28 -0400

    Fixed issue with data variable

--- front-end/counter.js
+++ front-end/counter.js
@@ -4,7 +4,7 @@ async function viewsCounter() {
     let response = await fetch('https://n2ssi4tgkrb6gk5jovgfeesu6i0cfcuf.lambda-url.us-east-1.on.aws/');
     if(response.ok){
         let data = await response.json();
-        counter.innerHTML = 'Views: ${data}';
+        counter.innerHTML = `Views: ${data}`;
     }
     else{
         console.log("HTTP-Error: " + response.status);
